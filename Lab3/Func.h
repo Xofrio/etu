9bbb6003a888dfa0a9a42da34f0294072e44d12c
Xofrio <54882123+Xofrio@users.noreply.github.com>
2019-10-08 17:31:54 +0300

    oooo, fancy!

--- Lab3/Func.h
+++ Lab3/Func.h
@@ -32,14 +32,14 @@ int getMenuOption() {
 		if (cin.fail()) {
 			cin.clear();
 			cin.ignore(cin.rdbuf()->in_avail());
-			cout << endl << "Попробуйте ввести корректную опцию (от 0 до 9): ";
+			cout << endl << "Попробуйте ввести корректную опцию (от 0 до 10): ";
 		}
-		else if (userNumber == 0 || userNumber == 1 || userNumber == 2 || userNumber == 3 || userNumber == 4 || userNumber == 5 || userNumber == 6 || userNumber == 7 || userNumber == 8 || userNumber == 9) {
+		else if (userNumber == 0 || userNumber == 1 || userNumber == 2 || userNumber == 3 || userNumber == 4 || userNumber == 5 || userNumber == 6 || userNumber == 7 || userNumber == 8 || userNumber == 9 || userNumber == 10) {
 			cin.ignore(cin.rdbuf()->in_avail());
 			return userNumber;
 		}
 		else
-			cout << endl << "Попробуйте ввести корректную опцию (от 0 до 9): ";
+			cout << endl << "Попробуйте ввести корректную опцию (от 0 до 10): ";
 	}
 }
 
@@ -58,6 +58,24 @@ bool getArrayMenuOption() {
 		}
 		else
 			cout << endl << "Попробуйте ввести корректную опцию (от 0 до 1): ";
+	}
+}
+
+int getSubMenuOption() {
+	while (true) {
+		int userNumber;
+		cin >> userNumber;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(cin.rdbuf()->in_avail());
+			cout << endl << "Попробуйте ввести корректную опцию (от 0 до 3): ";
+		}
+		else if (userNumber == 0 || userNumber == 1 || userNumber == 2 || userNumber == 3) {
+			cin.ignore(cin.rdbuf()->in_avail());
+			return userNumber;
+		}
+		else
+			cout << endl << "Попробуйте ввести корректную опцию (от 0 до 3): ";
 	}
 }
 
@@ -281,6 +299,47 @@ void secondaryDiagonalSymmetry(int **A, int order) {
 		cout << endl << "Матрица не симметрична относительно побочной диагонали." << endl;
 }
 
+void rowsSwap(int **A, int order) {
+	int tmp;
+	for (int i = 0; i < order / 2; ++i) {
+		for (int j = 0; j < order; ++j) {
+			tmp = A[i][j];
+			A[i][j] = A[order - 1 - i][j];
+			A[order - 1 - i][j] = tmp;
+		}
+	}
+}
+
+void swapClockwise(int **A, int order) {
+	int **B = 0;
+	B = new int *[order / 2];
+	for (int i = 0; i < order / 2; ++i)
+		B[i] = new int[order / 2];
+	for (int i = 0; i < order / 2; ++i) {
+		for (int j = 0; j < order / 2; ++j)
+			B[i][j] = A[i][j];
+	}
+	for (int i = 0; i < order / 2; ++i) {
+		for (int j = 0; j < order / 2; ++j)
+			A[i][j] = A[i + order / 2][j];
+	}
+	for (int i = order / 2; i < order; ++i) {
+		for (int j = 0; j < order / 2; ++j)
+			A[i][j] = A[i][j + order / 2];
+	}
+	for (int i = order / 2; i < order; ++i) {
+		for (int j = order / 2; j < order; ++j)
+			A[i][j] = A[i - order / 2][j];
+	}
+	for (int i = 0; i < order / 2; ++i) {
+		for (int j = order / 2, k = 0; j < order; ++j, ++k)
+			A[i][j] = B[i][k];
+	}
+	for (int i = 0; i < order / 2; ++i)
+		delete[] B[i];
+	delete[] B;
+}
+
 void generateArray(int **&A, int rows, int columns) {
 	A = new int *[rows];
 	for (int i = 0; i < rows; ++i)
@@ -290,6 +349,7 @@ void generateArray(int **&A, int rows, int columns) {
 			A[i][j] = rand() % 100;
 	}
 }
+
 
 void inputArray(int **&A, int rows, int columns) {
 	A = new int *[rows];
@@ -304,16 +364,6 @@ void inputArray(int **&A, int rows, int columns) {
 	cout << endl;
 }
 
-void rowsSwap(int **A, int order) {
-	int tmp;
-	for (int i = 0; i < order / 2; ++i) {
-		for (int j = 0; j < order; ++j) {
-			tmp = A[i][j];
-			A[i][j] = A[order - 1 - i][j];
-			A[order - 1 - i][j] = tmp;
-		}
-	}
-}
 
 void showArray(int **A, int rows, int columns) {
 	cout << '\t';
@@ -328,6 +378,64 @@ void showArray(int **A, int rows, int columns) {
 		SetConsoleTextAttribute(textColour, 7);
 		for (int j = 0; j < columns; ++j)
 			cout << A[i][j] << '\t';
+		cout << endl << endl;
+	}
+}
+
+void showFancyArray(int **A, int order, bool condition, int method) {
+	cout << '\t';
+	int part1 = 0, part2 = 0, part3 = 0, part4 = 0;
+	if (condition == 0) {
+			part1 = 3;
+			part2 = 4;
+			part3 = 9;
+			part4 = 10;
+	}
+	else if (method == 0) {
+		part1 = 9;
+		part2 = 3;
+		part3 = 10;
+		part4 = 4;
+	}
+	else if (method == 1) {
+		part1 = 10;
+		part2 = 9;
+		part3 = 4;
+		part4 = 3;
+	}
+	else if (method == 2) {
+		part1 = 9;
+		part2 = 10;
+		part3 = 3;
+		part4 = 4;
+	}
+	else if (method == 3) {
+		part1 = 4;
+		part2 = 3;
+		part3 = 10;
+		part4 = 9;
+	}
+	SetConsoleTextAttribute(textColour, 5);
+	for (int i = 0; i < order; ++i)
+		cout << i + 1 << '\t';
+	SetConsoleTextAttribute(textColour, 7);
+	cout << endl << endl;
+	for (int i = 0; i < order; ++i) {
+		SetConsoleTextAttribute(textColour, 5);
+		cout << i + 1 << '\t';
+		SetConsoleTextAttribute(textColour, 7);
+		for (int j = 0; j < order; ++j) {
+			if (i >= 0 && i < order /2 && j >= 0 && j < order /2)
+				SetConsoleTextAttribute(textColour, part1);
+			else if (i >= 0 && i < order / 2 && j >= order / 2 && j < order)
+				SetConsoleTextAttribute(textColour, part2);
+			else if (i >= order / 2 && i < order && j >= 0 && j < order /2)
+				SetConsoleTextAttribute(textColour, part3);
+			else if (i >= order / 2 && i < order && j >= order / 2 && j < order)
+				SetConsoleTextAttribute(textColour, part4);
+			cout << A[i][j] << '\t';
+		}
+		SetConsoleTextAttribute(textColour, 7);
 		cout << endl << endl;
 	}
 }
@@ -350,7 +458,8 @@ void Menu() {
 	cout << "6. Определить, является ли матрица симметричной относительно побочной диагонали" << endl;
 	cout << "7. Отсортировать матрицу по убыванию элементов первого столбца" << endl;
 	cout << "8. Поменять строки по принципу: первая с последней, вторая с предпоследней и т.д." << endl;
-	cout << "9. Выйти" << endl;
+	cout << "9. Поменять местами квадранты" << endl;
+	cout << "10. Выйти" << endl;
 	cout << "Опция: ";
 }
 
@@ -358,6 +467,15 @@ void arrayMenu() {
 	cout << "Выберите опцию . . ." << endl;
 	cout << "0. Заполнить матрицу вручную" << endl;
 	cout << "1. Заполнить матрицу автоматически" << endl;
+	cout << "Опция: ";
+}
+
+void swapMenu() {
+	cout << "Выберите опцию . . ." << endl;
+	cout << "0. Поменять квадранты местами по часовой стрелке" << endl;
+	cout << "1. Поменять квадранты местами крест-накрест" << endl;
+	cout << "2. Поменять квадранты местами вверх дном" << endl;
+	cout << "3. Поменять квадранты местами левой и правой стороной" << endl;
 	cout << "Опция: ";
 }
 
--- Lab3/Source.cpp
+++ Lab3/Source.cpp
@@ -3,7 +3,7 @@
 int main() {
 	setlocale(0, "");
 	srand((unsigned)time(NULL));
-	int **arrayOfNumbers = 0, menuOption, rows = 1, columns = 1;
+	int **arrayOfNumbers = 0, menuOption, subMenuOption, rows = 1, columns = 1;
 	bool condition = 1, created = 0, square, manual, prerecordedA = 0, prerecordedB = 0; //prerecorded is located here for secondaryDiagonal search if v.no.5 is executed, so v.no.6 is not full, till v.no.5 is done
 	generateArray(arrayOfNumbers, rows, columns);
 	while (condition) {
@@ -130,6 +130,56 @@ int main() {
 			break;
 		}
 		case 9: {
+			if (created == 0)
+				cout << endl << "Нет матрицы." << endl << endl;
+			else if (square == 0 || rows == 1 || rows % 2 == 1)
+				cout << endl << "Необходимо создать квадратную матрицу порядка , где N>=2 и N % 2 = 0" << endl << endl;
+			else {
+				cout << endl;
+				swapMenu();
+				subMenuOption = getSubMenuOption();
+				switch (subMenuOption) {
+				case 0: {
+					cout << endl << "Изачальная матрица:" << endl << endl;
+					showFancyArray(arrayOfNumbers, rows, 0, 0);
+					swapClockwise(arrayOfNumbers, rows);
+					cout << endl << "Матрица, квадранты которой поменялись местами по часовой стрелке:" << endl << endl;
+					showFancyArray(arrayOfNumbers, rows, 1, 0);
+					cout << endl;
+					break;
+				}
+				case 1: {
+					cout << endl << "Изачальная матрица:" << endl << endl;
+					showFancyArray(arrayOfNumbers, rows, 0, 1);
+					//swap x-x
+					cout << endl << "Матрица, квадранты которой поменялись местами крест-накрест:" << endl << endl;
+					showFancyArray(arrayOfNumbers, rows, 1, 1);
+					cout << endl;
+					break;
+				}
+				case 2: {
+					cout << endl << "Изачальная матрица:" << endl << endl;
+					showFancyArray(arrayOfNumbers, rows, 0, 2);
+					//swap updown
+					cout << endl << "Матрица, квадранты которой поменялись местами по часовой стрелке:" << endl << endl;
+					showFancyArray(arrayOfNumbers, rows, 1, 2);
+					cout << endl;
+					break;
+				}
+				case 3: {
+					cout << endl << "Изачальная матрица:" << endl << endl;
+					showFancyArray(arrayOfNumbers, rows, 0, 3);
+					//swap left-right
+					cout << endl << "Матрица, квадранты которой поменялись местами по часовой стрелке:" << endl << endl;
+					showFancyArray(arrayOfNumbers, rows, 1, 3);
+					cout << endl;
+					break;
+				}
+				}
+			}
+			break;
+		}
+		case 10: {
 			cout << endl;
 			condition = 0;
 			break;
