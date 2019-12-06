ec6a06f5758ed58dbfa82c13ce5f0e994740f22e
Xofrio <54882123+Xofrio@users.noreply.github.com>
2019-12-06 16:44:25 +0300

    ControlPoint

---
+++ CheckPoint/Func.h
@@ -0,0 +1,418 @@
+﻿#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+#include <conio.h>
+#include <vector>
+#include <Windows.h>
+#include <chrono>
+
+using namespace std;
+
+HANDLE textColour = GetStdHandle(STD_OUTPUT_HANDLE);
+
+#ifndef FUNC_H
+#define FUNC_H
+
+void swap(int *first, int *second) {
+	*first = *first + *second;
+	*second = *first - *second;
+	*first = *first - *second;
+}
+
+int getValue() {
+	while (true) {
+		int userNumber;
+		cin >> userNumber;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(cin.rdbuf()->in_avail());
+			cout << endl << "Попробуйте ввести корректное число: ";
+		}
+		else {
+			cin.ignore(cin.rdbuf()->in_avail());
+			return userNumber;
+		}
+	}
+}
+
+int getArrayValue() {
+	while (true) {
+		int userNumber;
+		cin >> userNumber;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(cin.rdbuf()->in_avail());
+			cout << endl << "Попробуйте ввести корректное число: ";
+		}
+		else if (userNumber >= 0 || userNumber <= 99) {
+			cin.ignore(cin.rdbuf()->in_avail());
+			return userNumber;
+		}
+		else {
+			cin.clear();
+			cin.ignore(cin.rdbuf()->in_avail());
+			cout << endl << "Попробуйте ввести корректное число: ";
+		}
+	}
+}
+
+void output(bool *A) {
+	for (int i = 31; i >= 0; --i) {
+		if ((31 - i) % 8 == 0 && i != 31)
+			cout << " ";
+		cout << A[i];
+		if (i == 31) {
+			cout << " ";
+			SetConsoleTextAttribute(textColour, 10);
+		}
+	}
+	SetConsoleTextAttribute(textColour, 7);
+}
+
+void charArray(char litera, bool *digits) {
+	for (int i = 31; i >= 0; --i)
+		digits[i] = int(litera) & ((bool)1 << i);
+}
+
+void bubbleSorting(int *A, int count) {
+	for (int i = 0; i < count; ++i) {
+		for (int j = 0; j < count - 1; ++j) {
+			if (A[j] > A[j + 1])
+				swap(&A[j], &A[j + 1]);
+		}
+	}
+}
+
+void cocktailSorting(int *A, int count) {
+	int left = 0, right = count - 1;
+	bool motion = 1;
+	while ((left < right) && motion == 1) {
+		motion = 0;
+		for (int i = left; i < right; ++i) {
+			if (A[i] > A[i + 1]) {
+				swap(&A[i], &A[i + 1]);
+				motion = 1;
+			}
+		}
+		--right;
+		for (int i = right; i > left; --i) {
+			if (A[i - 1] > A[i]) {
+				swap(&A[i - 1], &A[i]);
+				motion = 1;
+			}
+		}
+		++left;
+	}
+}
+
+void combSorting(int *A, int count) {
+	int gap = count;
+	bool motion = 1;
+	while (gap != 1 || motion == 1) {
+		gap = (gap * 10) / 13;
+		if (gap < 1)
+			gap = 1;
+		motion = 0;
+		for (int i = 0; i < count - gap; ++i) {
+			if (A[i] > A[i + gap]) {
+				swap(&A[i], &A[i + gap]);
+				motion = 1;
+			}
+		}
+	}
+}
+
+void insertionSorting(int *A, int count) {
+	int i, k, j;
+	for (i = 1; i < count; ++i) {
+		k = A[i];
+		j = i - 1;
+		while (j >= 0 && A[j] > k) {
+			A[j + 1] = A[j];
+			--j;
+		}
+		A[j + 1] = k;
+	}
+}
+
+void quickSorting(int *A, int start, int end) {
+	int i = start, j = end, tmp;
+	int separator = A[(start + end) / 2];
+	while (i <= j) {
+		while (A[i] < separator)
+			++i;
+		while (A[j] > separator)
+			--j;
+		if (i <= j) {
+			tmp = A[i];
+			A[i] = A[j];
+			A[j] = tmp;
+			++i;
+			--j;
+		}
+	}
+	if (start < j)
+		quickSorting(A, start, j);
+	if (i < end)
+		quickSorting(A, i, end);
+}
+
+void intArray(int number, bool *digits) {
+	for (int i = 31; i >= 0; --i)
+		digits[i] = number & ((bool)1 << i);
+}
+
+void inputArray(int *&A, int elements) {
+	for (int i = 0; i < elements; ++i) {
+		cout << "Введите значение элемента с индексами [" << i << "] (от 0 до 99): ";
+		A[i] = getArrayValue();
+	}
+	cout << endl;
+}
+
+void generateArray(int *A, int count) {
+	for (int i = 0; i < count; ++i)
+		A[i] = rand() % 100;
+}
+
+void inputArrayFromFile(int *&A, int &elements, bool &created) {
+	elements = 0;
+	ifstream in("array.txt");
+	if (in.is_open()) {
+		int temp;
+		while (!in.eof()) {
+			in >> temp;
+			++elements;
+		}
+		in.seekg(0, ios::beg);
+		in.clear();
+		A = new int[elements];
+		int i = 0;
+		while (!in.eof()) {
+			in >> A[i];
+			++i;
+		}
+		if (elements > 0)
+			created = 1;
+		else
+			created = 0;
+		in.close();
+	}
+	else {
+		cout << "Файл с именем 'array.txt' не найден." << endl << endl;
+	}
+}
+
+void showArray(int *A, int count) {
+	for (int i = 0; i < count; ++i)
+		cout << i << ". " << A[i] << endl;
+	cout << endl;
+}
+
+void draw(string str) {
+	SetConsoleTextAttribute(textColour, 2);
+	cout << str << endl;
+	SetConsoleTextAttribute(textColour, 7);
+}
+
+int mainMenu() {
+	int key = 0;
+	int code = 0;
+	do {
+		system("cls");
+		key = (key + 6) % 6;
+		if (key == 0)
+			draw(">Регистрация/вход");
+		else
+			cout << " Регистрация/вход" << endl;
+		if (key == 1)
+			draw(">Приветствие");
+		else
+			cout << " Приветствие" << endl;
+		if (key == 2)
+			draw(">Создать массив");
+		else
+			cout << " Создать массив" << endl;
+		if (key == 3)
+			draw(">Указать метод сортировки и получить некоторые данные о массиве");
+		else
+			cout << " Указать метод сортировки и получить некоторые данные о массиве" << endl;
+		if (key == 4)
+			draw(">Посмотреть флаги двух стран");
+		else
+			cout << " Посмотреть флаги двух стран" << endl;
+		if (key == 5)
+			draw(">Выйти");
+		else
+			cout << " Выйти" << endl;
+		code = _getch();
+		if (code != 13) {
+			code = _getch();
+			if (code == 80 || code == 251)
+				++key;
+			if (code == 72 || code == 246)
+				--key;
+		}
+	} while (code != 13);
+	system("cls");
+	return key;
+}
+
+int userMenu() {
+	int key = 0;
+	int code = 0;
+	do {
+		system("cls");
+		key = (key + 3) % 3;
+		if (key == 0)
+			draw(">Регистрация");
+		else
+			cout << " Регистрация" << endl;
+		if (key == 1)
+			draw(">Авторизация");
+		else
+			cout << " Авторизация" << endl;
+		if (key == 2)
+			draw(">Назад");
+		else
+			cout << " Назад" << endl;
+		code = _getch();
+		if (code != 13) {
+			code = _getch();
+			if (code == 80 || code == 251)
+				++key;
+			if (code == 72 || code == 246)
+				--key;
+		}
+	} while (code != 13);
+	system("cls");
+	return key;
+}
+
+int fallBack() {
+	int key = 0;
+	int code = 0;
+	do {
+		key = (key + 1) % 1;
+		if (key == 0){
+			cout << endl;
+			draw(">Назад");
+		}
+		code = _getch();
+	} while (code != 13);
+	return key;
+}
+
+int anonymousMenu() {
+	int key = 0;
+	int code = 0;
+	do {
+		system("cls");
+		key = (key + 3) % 3;
+		cout << "Обнаружен неавторизованный пользователь! Хотите авторизоваться?" << endl;
+		if (key == 0)
+			draw(">Авторизация");
+		else
+			cout << " Авторизация" << endl;
+		if (key == 1)
+			draw(">Я аноним");
+		else
+			cout << " Я аноним" << endl;
+		if (key == 2)
+			draw(">Назад");
+		else
+			cout << " Назад" << endl;
+
+		code = _getch();
+		if (code != 13) {
+			code = _getch();
+			if (code == 80 || code == 251)
+				++key;
+			if (code == 72 || code == 246)
+				--key;
+		}
+	} while (code != 13);
+	return key;
+}
+
+int arrayMenu() {
+	int key = 0;
+	int code = 0;
+	do {
+		system("cls");
+		key = (key + 4) % 4;
+		if (key == 0)
+			draw(">Заполнить массив вручную");
+		else
+			cout << " Заполнить массив вручную" << endl;
+		if (key == 1)
+			draw(">Заполнить массив автоматически");
+		else
+			cout << " Заполнить массив автоматически" << endl;
+		if (key == 2)
+			draw(">Считать массив из файла");
+		else
+			cout << " Считать массив из файла" << endl;
+		if (key == 3)
+			draw(">Назад");
+		else
+			cout << " Назад" << endl;
+		code = _getch();
+		if (code != 13) {
+			code = _getch();
+			if (code == 80 || code == 251)
+				++key;
+			if (code == 72 || code == 246)
+				--key;
+		}
+	} while (code != 13);
+	system("cls");
+	return key;
+}
+
+
+int methodMenu() {
+	int key = 0;
+	int code = 0;
+	do {
+		system("cls");
+		key = (key + 6) % 6;
+		if (key == 0)
+			draw(">Bubble");
+		else
+			cout << " Bubble" << endl;
+		if (key == 1)
+			draw(">Shaker");
+		else
+			cout << " Shaker" << endl;
+		if (key == 2)
+			draw(">Comb");
+		else
+			cout << " Comb" << endl;
+		if (key == 3)
+			draw(">Insert");
+		else
+			cout << " Insert" << endl;
+		if (key == 4)
+			draw(">Quick");
+		else
+			cout << " Quick" << endl;
+		if (key == 5)
+			draw(">Назад");
+		else
+			cout << " Назад" << endl;
+		code = _getch();
+		if (code != 13) {
+			code = _getch();
+			if (code == 80 || code == 251)
+				++key;
+			if (code == 72 || code == 246)
+				--key;
+		}
+	} while (code != 13);
+	system("cls");
+	return key;
+}
+
+#endif
---
+++ CheckPoint/Source.cpp
@@ -0,0 +1,401 @@
+﻿#include "Func.h"
+
+using namespace std;
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	srand((unsigned)time(NULL));
+	bool condition = 1, created = 0, *digits = 0, entered = 0, sorted = 0;
+	string login, rlogin, clogin, sortingMethod;
+	vector<char> pass, rpass;
+	char c, *cpass = 0;
+	int count = 32, groupNumber = 9894, elementsCount, *arrayOfNumbers = 0, sum = 0;
+	float avg;
+mainMenu:
+	while (condition) {
+		int answer = mainMenu();
+		switch (answer) {
+		case 0: {
+			int answer3 = userMenu();
+			switch (answer3) {
+			case 0: {
+				cout << "Введите логин: ";
+				cin >> rlogin;
+				cout << "Введите пароль: ";
+				while ((c = _getch()) != '\r') {
+					pass.push_back(c);
+					_putch('*');
+				}
+				ofstream flogin("login.txt");
+				flogin << rlogin;
+				flogin.close();
+				ofstream fpass("pass.txt");
+				for (size_t i = 0; i < pass.size(); ++i)
+					fpass << pass[i];
+				fpass.close();
+				cout << endl << "Регистрация прошла успешно!" << endl;
+				fallBack();
+				break;
+			}
+			case 1: {
+				ifstream flogin("login.txt");
+				while (!flogin.eof())
+					flogin >> clogin;
+				int passCharacters = 0;
+				ifstream in("pass.txt");
+				if (in.is_open()) {
+					int temp;
+					while (!in.eof()) {
+						in >> temp;
+						++passCharacters;
+					}
+					in.seekg(0, ios::beg);
+					in.clear();
+					cpass = new char[passCharacters];
+					int i = 0;
+					while (!in.eof()) {
+						in >> cpass[i];
+						++i;
+					}
+					in.close();
+				}
+				cout << "Введите логин: ";
+				cin >> login;
+				cout << "Введите пароль: ";
+				pass.clear();
+				vector<char>().swap(pass);
+				while ((c = _getch()) != '\r') {
+					pass.push_back(c);
+					_putch('*');
+				}
+				flogin.close();
+				char *examine = 0;
+				examine = new char[pass.size()];
+				for (size_t i = 0; i < pass.size(); ++i)
+					examine[i] = cpass[i];
+				cout << endl;
+				int comparison = 0;
+				for (size_t i = 0; i < pass.size(); ++i) {
+					if (examine[i] == pass[i])
+						++comparison;
+				}
+				bool win = 0;
+				if (comparison == pass.size())
+					win = 1;
+				if (login == clogin && win) {
+					cout << endl << "Авторизация прошла успешно! Здравствуйте, " << login << "!" << endl;
+					entered = 1;
+					fallBack();
+				}
+				else if (login != clogin) {
+					cerr << endl << "Пользователь не найден" << endl;
+					fallBack();
+				}
+				else if (win == 0) {
+					cerr << endl << "Неправильный пароль" << endl;
+					fallBack();
+				}
+				break;
+			}
+			case 2: goto mainMenu;
+			}
+			break;
+		}
+		case 1: {
+			int answer4;
+			if (entered == 0) {
+				answer4 = anonymousMenu();
+				switch (answer4) {
+				case 0: {
+					ifstream flogin("login.txt");
+					while (!flogin.eof())
+						flogin >> clogin;
+					int passCharacters = 0;
+					ifstream in("pass.txt");
+					if (in.is_open()) {
+						int temp;
+						while (!in.eof()) {
+							in >> temp;
+							++passCharacters;
+						}
+						in.seekg(0, ios::beg);
+						in.clear();
+						cpass = new char[passCharacters];
+						int i = 0;
+						while (!in.eof()) {
+							in >> cpass[i];
+							++i;
+						}
+						in.close();
+					}
+					cout << "Введите логин: ";
+					cin >> login;
+					cout << "Введите пароль: ";
+					pass.clear();
+					vector<char>().swap(pass);
+					while ((c = _getch()) != '\r') {
+						pass.push_back(c);
+						_putch('*');
+					}
+					flogin.close();
+					char *examine = 0;
+					examine = new char[pass.size()];
+					for (size_t i = 0; i < pass.size(); ++i)
+						examine[i] = cpass[i];
+					cout << endl;
+					int comparison = 0;
+					for (size_t i = 0; i < pass.size(); ++i) {
+						if (examine[i] == pass[i])
+							++comparison;
+					}
+					bool win = 0;
+					if (comparison == pass.size())
+						win = 1;
+					if (login == clogin && win) {
+						cout << endl << "Авторизация прошла успешно! Здравствуйте, " << login << "!" << endl;
+						entered = 1;
+						cout << "Приветствую тебя, " << login << ". На улице такая вьюга, присаживайся у очага, здесь тебе всегда рады." << endl;
+						cout << "Эту программу написал студент группы 9894 Белов Андрей." << endl;
+						digits = new bool[count];
+						intArray(groupNumber, digits);
+						cout << "Кстати, в памяти моего компьютера номер моей группы выглядит так: " << endl;
+						output(digits);
+						cout << endl << "А фамилия: " << endl;
+						char surname[5];
+						surname[0] = 'Б';
+						surname[1] = 'е';
+						surname[2] = 'л';
+						surname[3] = 'о';
+						surname[4] = 'в';
+						for (int i = 0; i < 5; ++i) {
+							charArray(surname[i], digits);
+							output(digits);
+							cout << " ";
+						}
+						cout << endl << endl;
+						fallBack();
+					}
+					else if (login != clogin) {
+						cout << endl << "Пользователь не найден" << endl;
+						goto notAuthorized;
+					}
+					else if (win == 0) {
+						cout << endl << "Неправильный пароль" << endl;
+						goto notAuthorized;
+					}
+					break;
+				}
+				case 1: {
+					cout << "Это ваше право, отныне вы будете именоваться пользователь N." << endl;
+				notAuthorized:
+					login = "пользователь N";
+					cout << "Приветствую тебя, " << login << ". На улице такая вьюга, присаживайся у очага, здесь тебе всегда рады." << endl;
+					cout << "Эту программу написал студент группы 9894 Белов Андрей." << endl;
+					digits = new bool[count];
+					intArray(groupNumber, digits);
+					cout << "Кстати, в памяти моего компьютера номер моей группы выглядит так: " << endl;
+					output(digits);
+					cout << endl << "А фамилия: " << endl;
+					char surname[5];
+					surname[0] = 'Б';
+					surname[1] = 'е';
+					surname[2] = 'л';
+					surname[3] = 'о';
+					surname[4] = 'в';
+					for (int i = 0; i < 5; ++i) {
+						charArray(surname[i], digits);
+						output(digits);
+						cout << " ";
+					}
+					cout << endl << endl;
+					fallBack();
+					break;
+				}
+				case 2: goto mainMenu;
+				}
+			}
+			else {
+			cout << "Приветствую тебя, " << login << ". На улице такая вьюга, присаживайся у очага, здесь тебе всегда рады." << endl;
+			cout << "Эту программу написал студент группы 9894 Белов Андрей." << endl;
+			digits = new bool[count];
+			intArray(groupNumber, digits);
+			cout << "Кстати, в памяти моего компьютера номер моей группы выглядит так: " << endl;
+			output(digits);
+			cout << endl << "А фамилия: " << endl;
+			char surname[5];
+			surname[0] = 'Б';
+			surname[1] = 'е';
+			surname[2] = 'л';
+			surname[3] = 'о';
+			surname[4] = 'в';
+			for (int i = 0; i < 5; ++i) {
+				charArray(surname[i], digits);
+				output(digits);
+				cout << " ";
+			}
+			cout << endl << endl;
+			fallBack();
+			}
+			break;
+		}
+		case 2: {
+			int answer2 = arrayMenu();
+			switch (answer2) {
+			case 0: {
+				cout << "Ввведите размерность массива:";
+				elementsCount = getValue();
+				arrayOfNumbers = new int[elementsCount];
+				inputArray(arrayOfNumbers, elementsCount);
+				showArray(arrayOfNumbers, elementsCount);
+				created = 1;
+				fallBack();
+				break;
+			}
+			case 1: {
+				cout << "Ввведите размерность массива:";
+				elementsCount = getValue();
+				arrayOfNumbers = new int[elementsCount];
+				generateArray(arrayOfNumbers, elementsCount);
+				showArray(arrayOfNumbers, elementsCount);
+				created = 1;
+				fallBack();
+				break;
+			}
+			case 2: {
+				inputArrayFromFile(arrayOfNumbers, elementsCount, created);
+				showArray(arrayOfNumbers, elementsCount);
+				fallBack();
+				break;
+			}
+			case 3: goto mainMenu;
+			}
+			break;
+		}
+		case 3: {
+			if (created == 0) {
+				cout << "Массив не создан." << endl;
+				fallBack();
+			}
+			else {
+				int method = methodMenu();
+				switch (method) {
+				case 0: {
+					auto begin = chrono::steady_clock::now();
+					bubbleSorting(arrayOfNumbers, elementsCount);
+					auto end = chrono::steady_clock::now();
+					sortingMethod = "Bubble";
+					cout <<"Время, затраченное на cортировку массива (метод сортировки - Bubble): " << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << " наносекунд" << endl << endl;
+					cout << endl << "Отсортированный массив (метод сортировки - Bubble):" << endl << endl;
+					sorted = 1;
+					break;
+				}
+				case 1: {
+					auto begin = chrono::steady_clock::now();
+					cocktailSorting(arrayOfNumbers, elementsCount);
+					auto end = chrono::steady_clock::now();
+					cout <<"Время, затраченное на сортировку массива (метод сортировки - Shaker): " << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << " наносекунд" << endl << endl;
+					cout << endl << "Отсортированный массив (метод сортировки - Shaker):" << endl << endl;
+					sorted = 1;
+					break;
+				}
+				case 2: {
+					auto begin = chrono::steady_clock::now();
+					combSorting(arrayOfNumbers, elementsCount);
+					auto end = chrono::steady_clock::now();
+					cout <<"Время, затраченное на сортировку массива (метод сортировки - Comb): " << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << " наносекунд" << endl << endl;
+					cout << endl << "Отсортированный массив (метод сортировки - Comb):" << endl << endl;
+					sorted = 1;
+					break;
+				}
+				case 3: {
+					auto begin = chrono::steady_clock::now();
+					insertionSorting(arrayOfNumbers, elementsCount);
+					auto end = chrono::steady_clock::now();
+					cout <<"Время, затраченное на сортировку массива (метод сортировки - Insert): " << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << " наносекунд" << endl << endl;
+					cout << endl << "Отсортированный массив (метод сортировки - Insert):" << endl << endl;
+					sorted = 1;
+					break;
+				}
+				case 4: {
+					auto begin = chrono::steady_clock::now();
+					quickSorting(arrayOfNumbers, 0, elementsCount - 1);
+					auto end = chrono::steady_clock::now();
+					cout <<"Время, затраченное на сортировку массива (метод сортировки - Quick): " << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() << " наносекунд" << endl << endl;
+					cout << endl << "Отсортированный массив (метод сортировки - Quick):" << endl << endl;
+					sorted = 1;
+					break;
+				}
+				case 5: goto mainMenu;
+				}
+				if (sorted == 1) {
+					showArray(arrayOfNumbers, elementsCount);
+					for (int i = 0; i < elementsCount; ++i)
+						sum = sum + arrayOfNumbers[i];
+					avg = (float)sum / (float)elementsCount;
+					cout << endl << "Среднее значение массива: " << avg << endl;
+					if (elementsCount % 2 == 1)
+						cout << "Медиана массива: " << arrayOfNumbers[elementsCount / 2] << endl;
+					else
+						cout << "Медиана массива: " << (arrayOfNumbers[elementsCount / 2 - 1] + arrayOfNumbers[elementsCount / 2]) / 2 << endl;
+					int arrayOfCount[100] = {};
+					for (int i = 0; i < elementsCount; ++i)
+						++arrayOfCount[arrayOfNumbers[i]];
+					int maxElement = arrayOfCount[0];
+					for (int i = 1; i < 100; ++i) {
+						if (maxElement < arrayOfCount[i])
+							maxElement = arrayOfCount[i];
+					}
+					int countSummary = 0;
+					cout << "Мода(ы) массива: ";
+					for (int i = 0; i < 100; ++i) {
+						if (arrayOfCount[i] == maxElement) {
+							cout << i << " ";
+							++countSummary;
+						}
+					}
+					cout << endl << "Количество мод: " << countSummary << ". Сколько раз мода(ы) встречаются в массиве: " << maxElement << endl;
+					fallBack();
+				}
+			}
+			break;
+		}
+		case 4: {
+			cout << "Флаг РФ: " << endl << endl;
+			SetConsoleTextAttribute(textColour, 240);
+			cout << "                  " << endl;
+			cout << "                  " << endl;
+			SetConsoleTextAttribute(textColour, 16);
+			cout << "                  " << endl;
+			cout << "                  " << endl;
+			SetConsoleTextAttribute(textColour, 64);
+			cout << "                  " << endl;
+			cout << "                  " << endl;
+			SetConsoleTextAttribute(textColour, 7);
+			cout << endl << endl;
+			cout << "Флаг Коста-Рики (16 вариант): " << endl << endl;
+			SetConsoleTextAttribute(textColour, 16);
+			cout << "                  " << endl;
+			SetConsoleTextAttribute(textColour, 240);
+			cout << "                  " << endl;
+			SetConsoleTextAttribute(textColour, 64);
+			cout << "                  " << endl;
+			cout << "                  " << endl;
+			SetConsoleTextAttribute(textColour, 240);
+			cout << "                  " << endl;
+			SetConsoleTextAttribute(textColour, 16);
+			cout << "                  " << endl;
+			SetConsoleTextAttribute(textColour, 7);
+			cout << endl;
+			fallBack();
+			break;
+		}
+		case 5: {
+			condition = 0;
+			break;
+		}
+		}
+	}
+	system("pause");
+	return 0;
+}
